fb10def73d3920b906ce9fc93f45b8c0107209af
Gui Sant <guisant@icloud.com>
2025-02-27 11:08:07 -0300

    Configurando rotas

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,74 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('admin.cliente');
 });
 
-Route::get('/empresa', function () {
-    return view('empresa');
+// Route::get('/empresa', function () {
+//     return view('empresa');
+// });
+
+Route::any('/any', function(){
+    return "permite todos acessos http";
+});
+
+Route::match(['get', 'post'], '/match', function(){
+    return "Permite somente acessos definidos";
+});
+
+route::get('/produto/{id}', function($id){
+    return "O id é: ".$id;
+});
+
+// Route::get('/sobre', function(){
+//     return redirect('empresa');
+// });
+
+// REDIRECIONANDO DIRETAMENTE DE ONDE PARA ONDE
+Route::redirect('/sobre', '/empresa');
+Route::view('/empresa', 'site/empresa');
+
+// COLOCANDO NOME NA ROTA
+Route::get('/news', function(){
+    return view('news');
+})->name('noticias');
+
+// ENVIANDO PARA A ROTA ATRAVÉS DO NOME
+Route::get('/novidades', function(){
+    return redirect()->route('noticias');
+});
+
+// ENCADEANDO ROTAS EM UM GRUPO ATRAVÉS DO PREFIX
+Route::prefix('admin')->group(function(){
+    Route::get('/', function(){
+        return "dashboards";
+    });
+
+    Route::get('/dashboards', function(){
+        return "dashboards";
+    });
+    
+    Route::get('/user', function(){
+        return "user";
+    });
+
+    Route::get('/clientes', function(){
+        return "clientes";
+    });
+});
+
+// ENCADEANDO ROTAS EM UM GRUPO ATRAVÉS DO NOME
+Route::name('admin.')->group(function(){
+
+    Route::get('admin/dashboards', function(){
+        return "dashboards";
+    })->name('dashboard');
+    
+    Route::get('admin/user', function(){
+        return "user";
+    })->name('usuario');
+
+    Route::get('admin/clientes', function(){
+        return 'clientes';
+})->name('cliente');
 });
